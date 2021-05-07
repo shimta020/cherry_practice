a5caa11618724671d34781f1d4e190082d435454
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-05-07 23:16:16 +0900

    improve:convert_lengthリファクタリングその2

--- cherry5/convert_length.rb
+++ cherry5/convert_length.rb
@@ -1,4 +1,4 @@
-def convert_length(length, unit_from, unit_to)
-  unit = {m: 1, ft: 3.28, in: 39.37}
-  (length/unit[unit_from]*unit[unit_to]).round(2)
+UNITS = {m: 1, ft: 3.28, in: 39.37}
+def convert_length(length, from: :m, to: :m)
+  (length/UNITS[from]*UNITS[to]).round(2)
 end
