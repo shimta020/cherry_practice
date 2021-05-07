d28d4d20484a35b8d98d7ac344010cbfcf7c82af
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-05-07 23:56:47 +0900

    add:optionハッシュ確認コード追加

---
+++ cherry5/sample.rb
@@ -0,0 +1,14 @@
+# currencies = {japan: 'yen', us: 'dollar', india: 'rupee'}
+# puts currencies.keys
+# puts currencies.values
+# puts currencies.has_key?(:japan)
+
+# a={japan: 'yen',**currencies}
+# puts a
+
+# def buy_burger(menu, drink: true, potato: true, **others)
+#   puts others
+# end
+
+# buy_burger('fish', drink: true, potato: false, salada: true, chicken: false)
+
