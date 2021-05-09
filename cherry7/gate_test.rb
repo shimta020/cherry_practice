96ae175abd9cabc10c66cfc69702eb07af5508b3
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-05-09 13:25:37 +0900

    test:ticket_test作成

--- cherry7/gate_test.rb
+++ cherry7/gate_test.rb
@@ -1,16 +1,23 @@
 require 'minitest/autorun'
 require './cherry7/gate'
+require './cherry7/ticket'
 
-class GateTest << Minitest::Test
-  def initialize
-    
-  end
-
+class GateTest < Minitest::Test
+  
   def test_gate
     umeda = Gate.new(:umeda)
     juso = Gate.new(:juso)
     ticket = Ticket.new(150)
     umeda.enter(ticket)
     assert juso.exit(ticket)
+  end
+
+  def test_umeda_to_mikuni_when_fare_is_not_enough
+    umeda = Gate.new(:umeda)
+    mikuni = Gate.new(:mikuni)
+
+    ticket = Ticket.new(150)
+    umeda.enter(ticket)
+    refute mikuni.exit(ticket)
   end
 end
