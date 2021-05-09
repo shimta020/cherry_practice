5b7fc3effa8c6c7f312975515c0034b47febaa0e
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-05-09 14:09:45 +0900

    add:gate,ticketにメソッド追加・完成

--- cherry7/gate.rb
+++ cherry7/gate.rb
@@ -1,13 +1,25 @@
 class Gate
+
+  STATIONS = %i(umeda juso mikuni)
+  FARES = [150, 190]
+
   def initialize(name)
     @name = name
   end
 
   def enter(ticket)
-    
+    ticket.stamp(@name)
   end
 
   def exit(ticket)
-    true
+    fare = calc_fare(ticket)
+    fare <= ticket.fare
+  end
+
+  def calc_fare(ticket)
+    from = STATIONS.index(ticket.stamp_at)
+    to = STATIONS.index(@name)
+    distance = to - from
+    FARES[distance - 1]
   end
 end
--- cherry7/ticket.rb
+++ cherry7/ticket.rb
@@ -1,5 +1,12 @@
 class Ticket
+
+  attr_reader :fare, :stamp_at
+
   def initialize(fare)
     @fare = fare
+  end
+
+  def stamp(name)
+    @stamp_at = name
   end
 end
