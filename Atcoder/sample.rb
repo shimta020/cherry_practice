7932dc2ebcb3aba5f8f07766798464e0aab36949
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-05-10 22:39:25 +0900

    AtCoder:Contest199 B

--- Atcoder/sample.rb
+++ Atcoder/sample.rb
@@ -1,7 +1,21 @@
-puts "入力してください"
- 
-## ユーザーのキーワード入力を待機
-input_val = gets
- 
-## 入力された文字を画面に表示
-puts "入力文字：" + input_val
+# row,line = gets.strip.split.map(&:to_i)
+# row_numbers = []
+# row.times do |n|
+#   row_numbers << gets.strip.split.map(&:to_i)
+# end
+# sum0 = row_numbers[0].inject(:+)
+# line.times do |n|
+#   sum0 += row_numbers[n-1][0]
+# end
+# result = sum0 - row_numbers[0][0]
+
+# https://atcoder.jp/contests/abc199/tasks/abc199_b
+
+n = gets.to_i
+nums = []
+2.times { nums << gets.strip.split.map(&:to_i) }
+result_nums = [*nums[0][0]..nums[1][0]]
+(n-1).times do |n|
+  result_nums &= [*nums[0][n+1]..nums[1][n+1]]
+end
+puts result_nums.size
