bb52d6fa413dcda7bbe69a89536dc043a48de891
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-05-11 00:13:17 +0900

    AtCoder:Contest199 Bリファクタリング

--- Atcoder/sample.rb
+++ Atcoder/sample.rb
@@ -11,11 +11,16 @@
 
 # https://atcoder.jp/contests/abc199/tasks/abc199_b
 
-n = gets.to_i
-nums = []
-2.times { nums << gets.strip.split.map(&:to_i) }
-result_nums = [*nums[0][0]..nums[1][0]]
-(n-1).times do |n|
-  result_nums &= [*nums[0][n+1]..nums[1][n+1]]
-end
-puts result_nums.size
+# n = gets.to_i
+# nums = []
+# 2.times { nums << gets.strip.split.map(&:to_i) }
+# result_nums = [*nums[0][0]..nums[1][0]]
+# (n-1).times do |n|
+#   result_nums &= [*nums[0][n+1]..nums[1][n+1]]
+# end
+# puts result_nums.size
+
+gets
+a = gets.split.map(&:to_i)
+b = gets.split.map(&:to_i)
+puts [0, b.min-a.max+1].max
