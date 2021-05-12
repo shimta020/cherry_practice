d42c99a7ba2e3ad78e1cfa46d1cadfac6262d5b3
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-05-12 18:19:13 +0900

    add:カレンダー問題作成(一部回答やヒント見ながら...)

--- OutPut/calendar.rb
+++ OutPut/calendar.rb
@@ -1,6 +1,20 @@
 require 'Date'
 today = Date.today
+month = today.month
+next_month = today.month + 1
+year = today.year
 day_of_week = ['Su','Mo','Tu','We','Th','Fr','Sa']
+first_date = Date.new(year, month, 1)
+last_date = Date.new(year, next_month, 1) - 1
 
-puts "#{today.strftime("%B")} #{today.strftime("%Y")}".center(20)
+puts "#{today.strftime("%B %Y")}".center(20)
 puts day_of_week.join(' ')
+
+(first_date..last_date).each_with_object(Array.new(7, '  ')) do |date, result|
+  result[date.wday] = date.day.to_s.rjust(2,' ')
+  if date.wday == 6
+    puts result.join(' ')
+  elsif date.day == last_date.day
+    puts result[0..date.wday].join(' ')
+  end
+end
