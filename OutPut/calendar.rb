620cc270cf50057e5cbf4123788d90e78781b3dc
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-05-12 17:29:04 +0900

    add:曜日表示実装

--- OutPut/calendar.rb
+++ OutPut/calendar.rb
@@ -1,6 +1,6 @@
 require 'Date'
 today = Date.today
+day_of_week = ['Su','Mo','Tu','We','Th','Fr','Sa']
 
-puts "#{today.strftime("%B")} #{today.strftime("%Y")}".center(30)
-
-
+puts "#{today.strftime("%B")} #{today.strftime("%Y")}".center(20)
+puts day_of_week.join(' ')
