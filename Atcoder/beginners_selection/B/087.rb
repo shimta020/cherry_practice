032d589f99f05c07bddee72d9e4451dd0c067564
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-05-13 22:00:58 +0900

    AtCoder:出力時ににselectを使わないよう改良

--- Atcoder/beginners_selection/B/087.rb
+++ Atcoder/beginners_selection/B/087.rb
@@ -5,12 +5,13 @@ a = gets.to_i
 b = gets.to_i
 c = gets.to_i
 x = gets.to_i
-sum = []
+result = 0
 (0..a).each do |p|
   (0..b).each do |q|
     (0..c).each do |r|
-      sum << 500*p + 100*q + 50*r
+      total = 500*p + 100*q + 50*r
+      result += 1 if total == x 
     end
   end
 end
-puts sum.select{|n| n ==x }.size
+puts result
