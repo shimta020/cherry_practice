804b343ea0a551ce757d56d3f3b2ee9f96d82339
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-05-14 01:17:01 +0900

    AtCoder:beginners_selection-083-Bのuniq使わないver

--- Atcoder/beginners_selection/B/085.rb
+++ Atcoder/beginners_selection/B/085.rb
@@ -2,5 +2,15 @@
 
 n = gets.to_i
 arr = n.times.map{ |n| gets.to_i }
-
+ 
 puts arr.uniq.size
+
+# uniq使わない別解
+# n = gets.chomp.to_i
+# values = []
+# n.times do |i|
+#   val = gets.chomp.to_i
+#   values.push(val) unless values.include?(val)
+# end
+ 
+# puts values.size
