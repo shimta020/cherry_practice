0a4b966cba2ccc184a4b0a13a154bca57033739e
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-05-15 18:53:38 +0900

    test:テストテンプレートをパス

--- OutPut/karaoke/karaoke.rb
+++ OutPut/karaoke/karaoke.rb
@@ -1,14 +1,15 @@
 # ruby ./OutPut/karaoke.karaoke.rb
-SCALE = %w(A A# B C C# D D# E F F# G G#)
 class KaraokeMachine
+
   def initialize(melody)
     @melody = melody
   end
 
   def transpose(amount)
-    @melody.split(/(\w#?)/).map { |m|
-      /(\w#?)/.match?(m) ? scales[(scales.find_index(m) + amount) % scales.size] : m
-    }.join
+    scales = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+    @melody.split(/(\w#?)/).map{
+            |m| /(\w#?)/.match?(m) ?
+            scales[(scales.find_index(m) + amount) % scales.length] : m 
+           }.join
   end
-
 end
