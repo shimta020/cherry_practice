b950bcfba33dc36b52d8603bbcae7a28f7583c91
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-05-17 16:01:53 +0900

    add:ボーナスドリンク別解(再帰関数を使った解答)

--- OutPut/bonus_drink/bonus_drink.rb
+++ OutPut/bonus_drink/bonus_drink.rb
@@ -1,9 +1,27 @@
-n = gets.to_i
-result = n
-tmp_empty_drink = n
-while tmp_empty_drink > 2
-  a,b = tmp_empty_drink.divmod(3)
-  tmp_empty_drink = a + b
-  result += a
+# 入力をコンソールで受け取って出力
+
+# n = gets.to_i
+# result = n
+# tmp_empty_drink = n
+# while tmp_empty_drink > 2
+#   a,b = tmp_empty_drink.divmod(3)
+#   tmp_empty_drink = a + b
+#   result += a
+# end
+# puts result
+
+# クラスを使って(再帰関数で)
+
+class Drink
+  def output(number)
+    number + bonus(number)
+  end
+
+  def bonus(number)
+    div, mod = number.divmod(3)
+    return 0 if div == 0
+    div + bonus(div + mod)
+  end
 end
-puts result
+
+puts Drink.new.output(100)
