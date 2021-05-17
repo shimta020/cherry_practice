45fab2594b4c41e153ab4130b3aab86e9b7cedbe
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-05-17 23:54:01 +0900

    improve:binary_search

--- sample2.rb
+++ sample2.rb
@@ -15,16 +15,38 @@
 
 nums = [1, 3, 5, 11, 12, 13, 17, 22, 25, 28]
 
-def binary_search(nums, num)
-  if nums[nums.length.div(2)] == num
-    return nums.length.div(2)
-  elsif nums[nums.length.div(2)] > num
-    nums = nums[nums.length.div(2)+1..nums.length-1]
-  else
-    nums = nums[0..nums.length.div(2)-1]
+# def binary_search(nums, num)
+#   if nums[nums.length.div(2)] == num
+#     return nums.length.div(2)
+#   elsif nums[nums.length.div(2)] > num
+#     nums = nums[nums.length.div(2)+1..nums.length-1]
+#   else
+#     nums = nums[0..nums.length.div(2)-1]
+#   end
+#   binary_search(nums, num)
+#   'None'
+# end
+
+# binary_search(nums, 11)
+
+def binary_search(nums, target)
+
+  head = 0
+  tail = nums.length - 1
+
+  while head <= tail
+    center = (head + tail).div(2)
+    if nums[center] = target
+      return center
+    elsif nums[center] > target
+      head = center + 1
+    else
+      tail = center - 1
+    end
   end
-  binary_search(nums, num)
+  
   'None'
+
 end
 
-binary_search(nums, 11)
+puts binary_search(nums, 12)
