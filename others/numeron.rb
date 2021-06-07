7dca0c5307c9bb8abaeef8f5594f346b5e6e0da4
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-06-07 16:33:15 +0900

    numeron:一旦完成

--- others/numeron.rb
+++ others/numeron.rb
@@ -10,7 +10,6 @@ class NUME
     num_1 = rand(0..9)
     num_2 = (arr - [num_1]).sample
     num_3 = (arr - [num_1] - [num_2]).sample
-    
     [num_1, num_2, num_3]
   end
 
@@ -18,7 +17,7 @@ class NUME
     while @eat != 3
       num = gets.strip.chars.map(&:to_i)
       num.zip(@tmp) do |x, y|
-        p [x,y]
+        # p [x,y]
         if x == y
           @eat += 1
         else
@@ -40,4 +39,3 @@ end
 
 nume = NUME.new
 nume.numeron
-nume.output
